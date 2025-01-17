7c28b1bc0a7e59288f153b200ca2f6b457057200
Evlen Fryling <evlen.fryling@gmail.com>
2025-01-17 15:19:21 -0500

    Final

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+console.log(7 + 3);
+console.log(7 - 3);
+console.log(7 + "3");
 
+console.log(4 - 8 * 2);
+console.log((4 - 8) * 2);
+
+console.log(10 / 6);
+console.log(10 % 6);
+
+console.log(5 == 5);
+console.log(5 == "5");
+console.log(5 === "5");
+
+console.log(6 || 3);
+console.log(3 || 6);
+console.log(6 && 3);
+console.log(3 && 3);
+*/
